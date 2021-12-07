baf98f84dfd907dd4aa0fdd982114973c3085231
Christian Svinth <christiansvinth@gmail.com>
2021-12-06 21:28:48 -0800

    Added importance sampling to timing script

--- CreditCardFraudNN.py
+++ CreditCardFraudNN.py
@@ -16,6 +16,7 @@ import time
 from torch.utils.data import TensorDataset, DataLoader
 from sklearn.metrics import classification_report, confusion_matrix
 from LRUCache import *
+import heapq
 
 CSV_PATH = "data/creditcard.csv"
 
@@ -70,8 +71,8 @@ class RemoteCacheDataset(Dataset):
     When a requested item is not in the shadow cache and must be "retreived from disk", a
     preset latency is added to retrieving that data item.
     """
-    def __init__(self, *tensors, eviction_method='lru'):
-        known_eviction_methods = ['random-replace', 'lru', 'never-evict']
+    def __init__(self, *tensors, eviction_method='lru', data_variance=1):
+        known_eviction_methods = ['random-replace', 'lru', 'never-evict', 'importance']
         
         
         if eviction_method not in known_eviction_methods:
@@ -89,13 +90,26 @@ class RemoteCacheDataset(Dataset):
         x = tuple(tensor[0] for tensor in self.tensors)
         self.eviction_method = eviction_method
 
+        # importance sampling cache and necessary values
+        self._data_variance = data_variance
+        self._cache_variance = 0
+        
+        if self.eviction_method is 'importance':
+            # use a standard list as a heap, and the heapq module to manage it
+            # which gives direct control over the heap contentsf
+            self.shadow_cache = []
+
         # initially seed memcached server with X number of values
         print("Dataset length: ", len(self.tensors[0]))
         print("Cache size: ", CACHE_SIZE)
         for i in range(CACHE_SIZE):
             self._write_cache(i, [tensors[0][i].tolist(), tensors[1][i].tolist()])
-            self.shadow_cache.insert(i)
-            #break
+
+            if self.eviction_method is 'importance':
+                heapq.heappush(self.shadow_cache, [1, i])
+            else:
+                self.shadow_cache.insert(i)
+            
         print(len(self.shadow_cache.lookup))
     def __getitem__(self, index):
         return self._query_cache(index)
@@ -137,13 +151,50 @@ class RemoteCacheDataset(Dataset):
                 #time.sleep(self.DISK_LATENCY)
                 # Directly pull the missing sample "from disk"
                 result = [self.tensors[0][index].tolist(), self.tensors[1][index].tolist()]
-
-            self.GLOBAL_CACHE_MISSES += 1
+            
+            elif self.eviction_method is 'importance':
+                # if we get a cache miss we need to perform the following operations:
+                #    1) select a random index in the queue
+                #    2) update the counter of the selected index to be the largest in the queue
+                #    3) evict LRU item and fetch the new item - should also increase its index so it won't
+                #       immediately get evicted from the cache
+                #    4) compute updated cache variance
+                substitue_index = random.randint(0, len(self.shadow_cache) - 1)
+                
+                largest_value = self.shadow_cache[self.cache_size-1][0]
+                self.shadow_cache[substitue_index][0] = largest_value + 1
+                # re-heapify in case we selected the least recently used item
+                heapq.heapify(self.shadow_cache)
+                
+                # get actual data from cache and scale - how to scale??
+                key_to_get = str(self.shadow_cache[substitue_index][1])
+                result = self.client.get(key_to_get)
+                # scale result by simple multiplication: this scale factor will likely be small given the initial
+                # variance of the overall dataset
+                result = [(np.array(result[0]) * self._cache_variance/self._data_variance).tolist(), result[1]]
+                
+                #key_to_remove = random.sample(self.shadow_cache, 1)[0]
+                # according to heapq, this is more efficient than explicitly doing a push/pop operation
+                heapq.heappushpop(self.shadow_cache, [largest_value+2, index])
+                self._write_cache(index, [self.tensors[0][index].tolist(), self.tensors[1][index].tolist()])
+                
+                # lastly recompute updated cache variance
+                self._compute_cache_variance()
+                    
+                self.GLOBAL_CACHE_MISSES += 1
             
         # result should now be in the form of a list with data as first item and output as second
 
         else:
             self.GLOBAL_CACHE_HITS += 1
+
+            if self.eviction_method is 'importance':
+                # need to update heap for shadow cache
+                for item in self.shadow_cache:
+                    if item[1] == index:
+                        item[0] +=1
+                        break
+                heapq.heapify(self.shadow_cache)
         item = tuple([torch.as_tensor(result[0]), torch.as_tensor(result[1])])
 
         return item
@@ -154,6 +205,17 @@ class RemoteCacheDataset(Dataset):
         #print(index, item)
         self.client.set(str(index), item)
         
+    def _compute_cache_variance(self):
+        # compute the current variance of the cache
+        # ideally this is something the cache server would do, however
+        # again due to limitations of using memcached, we perform this on the client side
+        temp_data = []
+        # slow and inefficient way to compute variance, but also easist to proof out
+        for i in range(self.cache_size):
+            temp_data.append(self.tensors[0][i].tolist())
+
+        temp_data = np.array(temp_data)
+        self._cache_variance = np.var(temp_data)
 
 
 
@@ -215,7 +277,7 @@ def train_model():
     
     X_train, X_test, y_train, y_test = train_test_split(X_tensor, y_tensor, test_size=0.2, random_state=1)
     
-    train_data = RemoteCacheDataset(X_train, y_train, eviction_method=args.eviction_method)
+    train_data = RemoteCacheDataset(X_train, y_train, eviction_method=args.eviction_method, data_variance=np.var(X))
     test_data = TensorDataset(X_test)
     train_sampler = RemoteCacheSampler(train_data)
     train_loader = DataLoader(dataset=train_data, batch_size=minibatch_size,
